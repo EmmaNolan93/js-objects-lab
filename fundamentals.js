3a6e117f25fa9da0a0d9464ce7052aeca9f9f493
Emma Nolan <emmanolan2019@gmail.com>
2022-09-15 12:28:17 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -8,6 +8,11 @@ const aCar = {
     ,
     registration: { year: 201, county: "WD", number: 1058 }
 };
-
+aCar.Millage = 10000;
+aCar.color = {
+    exterior: "red",
+    interiorFabric: { texture: "leather", shade: "cream" }
+  }
 console.log(aCar.owner + " drives a " + aCar.type.make);
 console.log( "Reg. = " + aCar.registration.year + "-" + aCar.registration.county +"-" +aCar.registration.number);
+console.log( "It is a " +  aCar.color.exterior + " car, " + aCar.milage +  " milage, with " + aCar.color.interior.texture + " interior.");
